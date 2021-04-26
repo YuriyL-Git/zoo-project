42052b371f33ba6a53d404256e7fd07786aef85c
Yuriy Lipchey <xaosmmm@gmail.com>
2021-04-26 17:51:19 +0300

    feat: implement caroosel for photo section

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,62 +1,48 @@
-/*======= Home page ===============*/
 const btnLeft = document.querySelector('.photo-section__btn-left');
 const btnRight = document.querySelector('.photo-section__btn-right');
-let itemActive = document.querySelector('.item-active');
 const items = document.querySelectorAll('.photo-section__item');
+
 let animationInProcess = false;
+let currentIndex = 0;
 
-let currentItem = 0;
-let previousItem = items.length;
-let nextItem = currentItem + 1;
+btnLeft.addEventListener('click', () => {
+  if (!animationInProcess) {
+    hideCurrent('to-left', currentIndex);
+    currentIndex = getFollowingIndex(currentIndex + 1);
+    showFollowing('from-right', currentIndex);
+  }
+});
 
-function updateItems(index) {
-  currentItem = (currentItem + items.length + index) % items.length;
-  nextItem = (currentItem + 1) % items.length;
-  previousItem = (currentItem - 1);
-  if (previousItem < 0) previousItem = items.length - 1;
+btnRight.addEventListener('click', () => {
+  if (!animationInProcess) {
+    hideCurrent('to-right', currentIndex);
+    currentIndex = getFollowingIndex(currentIndex - 1);
+    showFollowing('from-left', currentIndex);
+  }
+});
+
+function getFollowingIndex(index) {
+  return (items.length + index) % items.length;
 }
 
-function hideCurrent(direction, directionIndex) {
+function hideCurrent(direction, index) {
   animationInProcess = true;
-  items[currentItem].classList.add(direction);
-  items[currentItem].addEventListener('animationend', event => {
-    console.log('animation hide current ended');
-    items[currentItem].classList.remove('item-active', direction);
+  items[index].classList.add(direction);
+  items[index].addEventListener('animationend', event => {
+    items[index].classList.remove('item-active', direction);
     animationInProcess = false;
-
   });
 }
 
-function showFollowing(itemIndex, direction, directionIndex) {
-  items[itemIndex].classList.add('item-following', direction);
-  items[itemIndex].addEventListener('animationend', event => {
-    console.log('animation show following ended');
-    items[itemIndex].classList.remove('item-following', direction);
-    items[itemIndex].classList.add('item-active');
-
-    console.log('current=', currentItem);
-    console.log('next=', nextItem);
-    console.log('previous=', previousItem);
+function showFollowing(direction, index) {
+  items[index].classList.add('item-following', direction);
+  items[index].addEventListener('animationend', () => {
+    items[index].classList.remove('item-following', direction);
+    items[index].classList.add('item-active');
   });
 }
 
 
-btnLeft.addEventListener('click', event => {
-  if (!animationInProcess) {
-    hideCurrent('to-left', 1);
-    showFollowing(nextItem, 'from-right', 1);
-    updateItems(1);
-  }
-});
 
-btnRight.addEventListener('click', event => {
-
-  if (!animationInProcess) {
-    updateItems(-1);
-
-    hideCurrent('to-right', 1);
-    showFollowing(previousItem, 'from-left');
-  }
-});
 
 
