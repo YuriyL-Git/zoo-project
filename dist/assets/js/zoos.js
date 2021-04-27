aaab1f6a495017cd5ec1639232759d17a35533b5
Yuriy Lipchey <xaosmmm@gmail.com>
2021-04-27 16:36:13 +0300

    feat: add video play

--- dist/assets/js/zoos.js
+++ dist/assets/js/zoos.js
@@ -1,2 +1,32 @@
-/*============ Zoos Page ==============*/
-console.log('zoos page');
+const preview = [...document.querySelectorAll('.preview-container__title-wrapper')];
+const previewVideos = [...document.querySelectorAll('.preview-container__videos iframe')];
+const mainVideo = document.querySelector('.iframe-main');
+
+
+preview.forEach((preview, index) => {
+  preview.addEventListener('click', event => {
+    console.log(index);
+    //mainVideo.src = previewVideos[index].src;
+    console.log(mainVideo.src);
+    mainVideo.src = mainVideo.src.replace('?autoplay=1', '');
+    console.log(mainVideo.src);
+    [mainVideo.src, previewVideos[index].src] = [previewVideos[index].src, mainVideo.src];
+    console.log(mainVideo);
+    mainVideo.src += '?autoplay=1';
+    //mainVideo.src += "&autoplay=1";
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
