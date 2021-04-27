89e1136e0730365ab877aa3c1de84c83e59a5f2c
Yuriy Lipchey <xaosmmm@gmail.com>
2021-04-27 21:35:17 +0300

    refactor: refactor footer background images on zoos pages

--- dist/assets/js/zoos.js
+++ dist/assets/js/zoos.js
@@ -1,3 +1,5 @@
+/*================ Carousel videos =====================================*/
+
 const preview = [...document.querySelectorAll('.preview-container__title-wrapper')];
 const previewVideos = [...document.querySelectorAll('.preview-container__videos iframe')];
 const mainVideo = document.querySelector('.iframe-main');
@@ -5,17 +7,20 @@ const mainVideo = document.querySelector('.iframe-main');
 
 preview.forEach((preview, index) => {
   preview.addEventListener('click', event => {
-    console.log(index);
-    //mainVideo.src = previewVideos[index].src;
-    console.log(mainVideo.src);
     mainVideo.src = mainVideo.src.replace('?autoplay=1', '');
-    console.log(mainVideo.src);
     [mainVideo.src, previewVideos[index].src] = [previewVideos[index].src, mainVideo.src];
-    console.log(mainVideo);
     mainVideo.src += '?autoplay=1';
-    //mainVideo.src += "&autoplay=1";
   });
 });
+
+/*================ Read more btn =====================================*/
+const btnRead = document.querySelector('.btn-read-less');
+
+btnRead.addEventListener('click', () => {
+  console.log('clicked');
+});
+
+
 
 
 
